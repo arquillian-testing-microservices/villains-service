713a8cd7a91aa2cb0888c5079cc71efa92c2e5a6
Alex Soto <asotobu@gmail.com>
2017-07-12 10:56:38 +0200

    Adds URL constructor to CrimesGateway

--- src/main/java/org/lordofthejars/villains/villain/CrimesGateway.java
+++ src/main/java/org/lordofthejars/villains/villain/CrimesGateway.java
@@ -3,6 +3,7 @@ package org.lordofthejars.villains.villain;
 import io.vertx.core.json.JsonArray;
 import io.vertx.rxjava.ext.web.client.HttpResponse;
 import io.vertx.rxjava.ext.web.client.WebClient;
+import java.net.URL;
 import rx.Single;
 
 public class CrimesGateway {
@@ -10,6 +11,10 @@ public class CrimesGateway {
     private WebClient client;
     private String host;
     private Integer port;
+
+    public CrimesGateway(WebClient webClient, URL url) {
+        this(webClient, url.getHost(), url.getPort());
+    }
 
     public CrimesGateway(WebClient webClient, String host, Integer port) {
         this.client = webClient;
