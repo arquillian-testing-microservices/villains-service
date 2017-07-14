2dba78e19dd1ec6b4668aca21e7c53d5e9af43a1
Alex Soto <asotobu@gmail.com>
2017-07-14 09:34:26 +0200

    Initial solution

--- src/test/java/org/lordofthejars/villains/villain/CrimesConsumerContractTest.java
+++ src/test/java/org/lordofthejars/villains/villain/CrimesConsumerContractTest.java
@@ -1,26 +1,54 @@
 package org.lordofthejars.villains.villain;
 
+import au.com.dius.pact.consumer.dsl.PactDslWithProvider;
+import au.com.dius.pact.model.RequestResponsePact;
 import io.vertx.core.DeploymentOptions;
 import io.vertx.core.Vertx;
 import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
+import io.vertx.rxjava.ext.web.client.WebClient;
+import java.net.URL;
 import java.util.concurrent.CountDownLatch;
+import org.arquillian.algeron.consumer.StubServer;
+import org.arquillian.algeron.pact.consumer.spi.Pact;
+import org.arquillian.algeron.pact.consumer.spi.PactVerification;
+import org.jboss.arquillian.junit.Arquillian;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import rx.Single;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.tuple;
 
+@RunWith(Arquillian.class)
 public class CrimesConsumerContractTest {
 
     private static Vertx vertx;
 
+    @StubServer
+    URL pactServer;
+
+    @Pact(provider = "crimes", consumer = "villains")
+    public RequestResponsePact returnListOfCrimes(PactDslWithProvider builder) {
+
+        return builder
+            .uponReceiving("Gru villain to get all Crimes")
+            .path("/crimes/Gru")
+            .method("GET")
+            .willRespondWith()
+            .status(200)
+            .body(RESPONSE)
+            .toPact();
+    }
+
     @Test
+    @PactVerification("crimes")
     public void should_get_list_of_crimes_by_villain() {
 
         // given
-        CrimesGateway crimesGateway = new CrimesGateway();
+        final WebClient webClient = WebClient.create(new io.vertx.rxjava.core.Vertx(vertx));
+        CrimesGateway crimesGateway = new CrimesGateway(webClient, pactServer);
 
         // when
         final Single<JsonArray> gruCrimes = crimesGateway.getCrimesByVillainName("Gru");
@@ -53,4 +81,17 @@ public class CrimesConsumerContractTest {
         }).start();
         waitVerticleDeployed.await();
     }
+
+    private static String RESPONSE = "[\n"
+        + "    {\n"
+        + "        \"name\": \"Moon\",\n"
+        + "        \"villain\": \"Gru\",\n"
+        + "        \"wiki\": \"https://en.wikipedia.org/wiki/Moon\"\n"
+        + "    },\n"
+        + "    {\n"
+        + "        \"name\": \"Times Square JumboTron\",\n"
+        + "        \"villain\": \"Gru\",\n"
+        + "        \"wiki\": \"https://en.wikipedia.org/wiki/One_Times_Square\"\n"
+        + "    }\n"
+        + "]";
 }
